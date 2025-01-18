dcfa4c0309c2da904c33996de1d442cf18c241e1
juli0mendes <jmendes@live.com>
2025-01-18 17:18:16 -0300

    feature: added two account into organization

--- organizations/organization.tf
+++ organizations/organization.tf
@@ -1,3 +1,25 @@
 resource "aws_organizations_organization" "organization" {
   feature_set = "ALL"
 }
+
+resource "aws_organizations_account" "dev_account" {
+  name      = "dev-juli0mendes"
+  email     = "juuliomendes2@gmail.com"
+  role_name = "OrganizationAccountAccessRole"
+
+  tags = {
+    Environment = "Development"
+    Project     = "AWS Organization Automation"
+  }
+}
+
+resource "aws_organizations_account" "prod_account" {
+  name      = "prod-juli0mendes"
+  email     = "juuliomendesitau@gmail.com"
+  role_name = "OrganizationAccountAccessRole"
+
+  tags = {
+    Environment = "Production"
+    Project     = "AWS Organization Automation"
+  }
+}
