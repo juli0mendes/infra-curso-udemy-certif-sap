84342ce3122e37a25e3cb10718b53c719aff71df
juli0mendes <jmendes@live.com>
2025-01-20 22:31:40 -0300

    feat: added policy iam - scp

--- organizations/organization.tf
+++ organizations/organization.tf
@@ -42,3 +42,26 @@ resource "aws_organizations_organizational_unit" "rio_de_janeiro_ou" {
   name      = "Rio de Janeiro"
   parent_id = aws_organizations_organization.organization.roots[0].id
 }
+
+resource "aws_organizations_policy" "allow_all" {
+  name        = "AllowAllPolicy"
+  description = "Allows all actions"
+  content     = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "*",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+  type = "SERVICE_CONTROL_POLICY"
+}
+
+resource "aws_organizations_policy_attachment" "sysop_allow_all" {
+  policy_id = aws_organizations_policy.allow_all.id
+  target_id = aws_organizations_account.sysop_account.id
+}
