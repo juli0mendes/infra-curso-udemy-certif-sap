cc6874d6fc5e34c1d92e5364ead0620383cacae8
juli0mendes <jmendes@live.com>
2025-03-07 17:51:11 -0300

    add rule for dynamic execution of apply and destroy steps

--- main.tf
+++ main.tf
@@ -9,3 +9,10 @@ module "iam_project" {
 module "organizations_project" {
   source = "./organizations-project"
 }
+
+resource "null_resource" "example" {
+  count = var.run_terraform_apply ? 1 : 0
+  provisioner "local-exec" {
+    command = "echo Run Terraform Apply: ${var.run_terraform_apply}"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "run_terraform_apply" {
+  description = "Control whether to run terraform apply or destroy"
+  type        = bool
+  default     = true
+}
