6417e12c0f3e5b90df6f06642153d021ea9c5b64
juli0mendes <jmendes@live.com>
2025-03-07 18:04:25 -0300

    feat: improved data type variable

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "run_terraform_apply" {
   description = "Control whether to run terraform apply or destroy"
-  type        = bool
-  default     = true
+  type        = string
+  default     = "true"
 }
