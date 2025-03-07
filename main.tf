c979cb6108ffb802e2487b0ca2852cbfa44be85f
juli0mendes <jmendes@live.com>
2025-03-07 17:13:37 -0300

    ci: improved execution of apply and destroy steps

--- main.tf
+++ main.tf
@@ -11,3 +11,11 @@ module "organizations_project" {
   source = "./organizations-project"
   count  = var.enable_organizations_project ? 1 : 0
 }
+
+# Use a variável run_terraform_apply em algum lugar para garantir que ela seja considerada
+resource "null_resource" "example" {
+  count = var.run_terraform_apply ? 1 : 0
+  provisioner "local-exec" {
+    command = "echo Run Terraform Apply: ${var.run_terraform_apply}"
+  }
+}
