e5accbbc7896e31ae291918a7d6a97d9d5c78f62
juli0mendes <jmendes@live.com>
2025-03-07 17:44:04 -0300

    feat: removed conditionl for apply or destroy

--- main.tf
+++ main.tf
@@ -9,11 +9,3 @@ module "iam_project" {
 module "organizations_project" {
   source = "./organizations-project"
 }
-
-# Use a variável run_terraform_apply em algum lugar para garantir que ela seja considerada
-resource "null_resource" "example" {
-  count = var.run_terraform_apply ? 1 : 0
-  provisioner "local-exec" {
-    command = "echo Run Terraform Apply: ${var.run_terraform_apply}"
-  }
-}
--- outputs.tf
+++
@@ -1,3 +0,0 @@
-output "run_terraform_apply" {
-  value = var.run_terraform_apply
-}
