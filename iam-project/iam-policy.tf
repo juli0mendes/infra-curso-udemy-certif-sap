1e896bebbc56c5b4574ba062c33e79319a1baf14
juli0mendes <jmendes@live.com>
2025-03-09 19:18:22 -0300

    feat: create policy for s3 access (abac)

--- iam-project/iam-policy.tf
+++ iam-project/iam-policy.tf
@@ -12,8 +12,16 @@ resource "aws_iam_group_policy_attachment" "so_support_user" {
   policy_arn = "arn:aws:iam::aws:policy/AWSSupportAccess"
 }
 
+resource "aws_iam_group_policy_attachment" "so_s3_access" {
+  group      = aws_iam_group.groups["so"].name
+  policy_arn = aws_iam_policy.s3_access.arn
+}
+
 resource "aws_iam_user" "julio1" {
   name = "julio1"
+  tags = {
+    department = "code"
+  }
 }
 
 resource "aws_iam_user_group_membership" "julio1_membership" {
@@ -29,4 +37,35 @@ resource "aws_iam_user_login_profile" "julio1_login_profile" {
 output "julio1_password" {
   value     = aws_iam_user_login_profile.julio1_login_profile.password
   sensitive = true
+}
+
+// para visualizar a senha gerada: terraform output -json julio1_password
+
+data "aws_iam_policy_document" "s3_access_policy" {
+  statement {
+    sid = "1"
+
+    actions = [
+      "s3:*"
+    ]
+
+    resources = [
+      "arn:aws:s3:::*"
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:PrincipalTag/department"
+      values = [
+        "code"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_policy" "s3_access" {
+  name        = "s3-access"
+  path        = "/"
+  description = "Policy to provide S3 access"
+  policy      = data.aws_iam_policy_document.s3_access_policy.json
 }
---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "julio1_password" {
+  value     = module.iam_project.julio1_password
+  sensitive = true
+}
