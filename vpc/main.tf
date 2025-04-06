21d395065135e0fa61fe73847db48c5ba7cfe955
juli0mendes <jmendes@live.com>
2025-04-06 15:01:33 -0300

    feat: removed remote-backend and and created two subnets (public and private)

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,8 +1,30 @@
 resource "aws_vpc" "vpc_a" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
     "Name" = "vpc-a"
   }
 }
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.vpc_a.id
+  cidr_block              = "10.0.16.0/20"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1b"
+
+  tags = {
+    "Name" = "subnet-a-public"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.vpc_a.id
+  cidr_block        = "10.0.32.0/20"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    "Name" = "subnet-b-private"
+  }
+}
+
