c158d1dd8f727890262e32928e9e7fb6d61f02e4
juli0mendes <jmendes@live.com>
2025-04-19 20:41:15 -0300

    feat: added internet gtw for vpc-a

--- vpc/main.tf
+++ vpc/main.tf
@@ -53,3 +53,11 @@ resource "aws_route_table_association" "private_subnet_association" {
   subnet_id      = aws_subnet.private_subnet.id
   route_table_id = aws_route_table.private_route_table.id
 }
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc_a.id
+
+  tags = {
+    "Name" = "igtw-vpc-a"
+  }
+}
