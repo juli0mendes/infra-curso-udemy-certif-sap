bb7a0f997093d194ba2e72a50d9755694dd46ee3
juli0mendes <jmendes@live.com>
2025-04-19 20:48:33 -0300

    feat: joined internet gateway in public route table for internet access

--- vpc/main.tf
+++ vpc/main.tf
@@ -61,3 +61,10 @@ resource "aws_internet_gateway" "internet_gateway" {
     "Name" = "igtw-vpc-a"
   }
 }
+
+resource "aws_route" "public_route" {
+  route_table_id         = aws_route_table.public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.internet_gateway.id
+}
+
