8fb3a231b69e00fb8e8cfb0e3bd8a05d8119aa18
juli0mendes <jmendes@live.com>
2025-04-24 22:30:29 -0300

    refactor: separating resources into files (single responsibility)

--- main.tf
+++ main.tf
@@ -10,9 +10,9 @@ provider "aws" {
 #   source = "./organizations-project"
 # }
 
-module "directory_service" {
-  source = "./directory-service"
-}
+# module "directory_service" {
+#   source = "./directory-service"
+# }
 
 # module "vpc" {
 #   source = "./vpc"
